61bba1b72da4e87757381c9c7e00ca033bc53b67
Dr. Bob <105605805+Dr-Bob-isTheBestDoctor@users.noreply.github.com>
2026-09-03 12:05:32 +0200

    enemyspawner verbeterd

--- Assets/Scripts/EnemySpawnerScript.cs
+++ Assets/Scripts/EnemySpawnerScript.cs
@@ -1,17 +1,35 @@
 using Mono.Cecil.Cil;
 using Unity.Mathematics;
 using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
 
 public class EnemySpawnerScript : MonoBehaviour
 {
+    public List<GameObject> Enemies;
     public float SpawnRadius = 10;
     public GameObject prefab;
     public quaternion SpawnRotation = new quaternion(0, 0, 0, 0);
+    void SpawnWave()
+    {
+        int i = 5;
+        
+        while(i != 0)
+        {
+            Vector3 position = new Vector3(UnityEngine.Random.Range(-SpawnRadius, SpawnRadius), UnityEngine.Random.Range(-SpawnRadius, SpawnRadius), UnityEngine.Random.Range(-SpawnRadius, SpawnRadius));
+            Enemies.Add(Instantiate(prefab, position, SpawnRotation));
+            i = i-1;
+        }
+        
+    }
 
-    // Update is called once per frame
     void FixedUpdate()
     {
-        Vector3 position = new Vector3(UnityEngine.Random.Range(-SpawnRadius, SpawnRadius), UnityEngine.Random.Range(-SpawnRadius, SpawnRadius), UnityEngine.Random.Range(-SpawnRadius, SpawnRadius));
-        Instantiate(prefab, position, SpawnRotation);
+        
+        if (Enemies.Count == 0)
+        {
+            SpawnWave();
+        }
+        
     }
 }
