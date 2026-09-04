5bc330b5b2b5a428bb8070e1692899485381efb4
Dr. Bob <105605805+Dr-Bob-isTheBestDoctor@users.noreply.github.com>
2026-09-04 10:47:16 +0200

    Game Over scherm

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -1,7 +1,18 @@
+using System.Runtime.InteropServices;
 using UnityEngine;
 
 public class Bullet : MonoBehaviour
-{     
+{
+    public float MaxProjectileLifetime = 50;
+    public float ProjectileLifetime;
+    private void Update()
+    {
+        ProjectileLifetime = ProjectileLifetime + Time.deltaTime;
+        if(ProjectileLifetime >= MaxProjectileLifetime)
+        {
+            Destroy(gameObject);
+        }
+    }
     private void OnCollisionEnter(Collision collision)
     {
         Destroy(gameObject);
