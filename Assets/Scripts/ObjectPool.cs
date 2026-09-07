98901603f7b049f772ba88cbcdf52930a4553748
Dr. Bob <105605805+Dr-Bob-isTheBestDoctor@users.noreply.github.com>
2026-09-07 12:04:46 +0200

    Jheffreighy Enemy

--- Assets/Scripts/ObjectPool.cs
+++ Assets/Scripts/ObjectPool.cs
@@ -1,42 +1,69 @@
-using UnityEngine;
-using Unity.Mathematics;
-using System.Collections;
 using System.Collections.Generic;
-using System.Runtime.CompilerServices;
+using UnityEngine;
 
 public class ObjectPool : MonoBehaviour
 {
     public static ObjectPool SharedInstance;
+
     public List<GameObject> pooledObjects;
     public GameObject objectToPool;
-    public int amountToPool;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public int amountToPool = 10;
+    public bool shouldExpand = true; // Optional: auto-expand pool if empty
+
     void Awake()
     {
-        SharedInstance = this;
+        if (SharedInstance == null)
+        {
+            SharedInstance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
     }
-    
+
     void Start()
     {
         pooledObjects = new List<GameObject>();
-        GameObject tmp;
-        for(int i = 0; i < amountToPool; i++)
+
+        // Populate pool initially
+        for (int i = 0; i < amountToPool; i++)
         {
-            tmp = Instantiate(objectToPool);
+            GameObject tmp = Instantiate(objectToPool);
             tmp.SetActive(false);
             pooledObjects.Add(tmp);
         }
-        
     }
+
     public GameObject GetPooledObject()
     {
-        for(int i = 0; i < amountToPool; i++)
+        // Iterate over the actual list count (not hardcoded amountToPool)
+        for (int i = 0; i < pooledObjects.Count; i++)
         {
-            if(!pooledObjects[i].activeInHierarchy)
+            // 1. Clean up missing/destroyed references safely
+            if (pooledObjects[i] == null)
+            {
+                pooledObjects.RemoveAt(i);
+                i--; // Adjust index after removal
+                continue;
+            }
+
+            // 2. Return an available inactive object
+            if (!pooledObjects[i].activeInHierarchy)
             {
                 return pooledObjects[i];
             }
         }
+
+        // 3. Dynamically expand pool if enabled and all objects are active
+        if (shouldExpand)
+        {
+            GameObject tmp = Instantiate(objectToPool);
+            tmp.SetActive(false);
+            pooledObjects.Add(tmp);
+            return tmp;
+        }
+
         return null;
     }
 }
